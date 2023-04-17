59e57088f2f40f009827ebd0d1fb0a23270cb6bf
LohanIsaque <126782104+LohanIsaque@users.noreply.github.com>
2023-04-17 10:51:51 -0300

    Add files via upload

---
+++ Lista.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Lista {
+    public static void main (String []args){
+        String [] alunos = new String[4];
+        alunos[0]="Sade";
+        alunos[1]="Alexus";
+        alunos[2]="Sam";
+        alunos[3]="Koma";
+
+        double[] mathScores = new double[4];
+        mathScores[0] = 94.5;
+        mathScores[2] = 76.8;
+
+        System.out.println("The number of students in the class is " + alunos.length);
+    }
+}
